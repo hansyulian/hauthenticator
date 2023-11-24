7fa801d43b5d43bf697fb978890b4825681525c8
Hans Yulian <hansyulian@windowslive.com>
2023-11-24 14:01:40 +0700

    fixed git ignore

---
+++ src/components/SyncProgressBar.tsx
@@ -0,0 +1,24 @@
+import { useSyncContext } from "@hooks/useSyncContext"
+import { useEffect } from "react";
+import { ProgressBar } from "react-native-paper"
+import { StyleSheet } from "react-native";
+
+export const SyncProgressBar = () => {
+  const { state } = useSyncContext();
+
+  useEffect(() => {
+    console.log("sync state", state);
+  }, [state])
+  if (state !== "SYNCING") {
+    return null;
+  }
+  return <ProgressBar indeterminate style={styles.progressBar} />
+}
+
+const styles = StyleSheet.create({
+  progressBar: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+  }
+})
